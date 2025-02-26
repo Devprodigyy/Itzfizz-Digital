e78a511886b1f5b002387fbda3a221ae039a50f2
kunalwatulkar <kunalwatulkar1@gmail.com>
2025-02-26 23:55:52 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Internship Project - PHASE
+# Original : https://www.pha5e.com/
+
+## About Me
+Hi, I'm **Kunal Watulkar**, a passionate frontend developer and founder of **DevProdigy**. I specialize in crafting modern, high-performance web applications and have experience with technologies like **React, Next.js, Tailwind CSS, GSAP, Three.js, and more**. I'm actively involved in tech communities like **Sheryians Coding School, Google Developers Club, and Code Your Chai**.
+
+### Connect with Me:
+- **Portfolio:** [devprodigy.in](https://devprodigy.in)
+- **GitHub:** [Devprodigyy](https://github.com/Devprodigyy)
+- **YouTube:** [DevProdigy](https://www.youtube.com/@Dev_Prodigy)
+- **LinkedIn:** [Kunal Watulkar](https://www.linkedin.com/in/devprodigy/)
+- **Twitter:** [@devprodigy__](https://twitter.com/devprodigy__)
+- **Email:** kunalwatulkar1@gmail.com
+
+## License
+This project is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,243 @@
+var imgs = document.querySelectorAll(".image-containers1 img");
+var img2 = document.querySelectorAll(".image-containers2 img");
+var img3 = document.querySelectorAll(".image-containers3 img");
+var img4 = document.querySelectorAll(".image-containers4 img");
+var info = document.querySelectorAll(".info");
+var headings = document.querySelectorAll(".txt");
+
+gsap.to("#loader", {
+  delay:2,
+  duration: 5, // Duration of the animation
+  top:"-100%"
+
+  
+}, code());
+
+
+function code(){
+  
+
+gsap.from(".imgcont",{
+  scale:0,
+  duration: 0.5,
+  ease: "power2.inOut",
+  stagger:1,
+  ease: "power2.out",
+ 
+})
+
+gsap.from(headings, {
+opacity: 0, // Start with opacity 0
+y: "100%", // Move 30 pixels down
+duration: 1, // Duration of the animation
+ease: "power2.inOut", // Easing function
+zIndex: 999,
+});
+
+imgs.forEach(function (img) {
+img.addEventListener("mouseover", function (e) {
+  // console.log("Mouse over image: " + e.target.src);
+  e.target.src =
+    "https://2022-pha5e-website-prod.s3.eu-west-3.amazonaws.com/assets/map-interactive.png"; // Change the image source
+  gsap.to(img, {
+    display: "block", // Show the image
+    opacity: 1, // Smoothly fade out the image instead of hiding it
+    duration: 0.3,
+    ease: "power2.out",
+    zIndex: 999, // Move the image to the back to prevent it from being covered by the text
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "transparent", // Make text transparent
+    WebkitTextStroke: "0.5px #4e4d4d", // Add stroke effect
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+
+img.addEventListener("mouseleave", function (e) {
+  e.target.src = " "; // Change the image source
+
+  gsap.to(img, {
+    opacity: 1, // Fade the image back in
+    duration: 0.3,
+    ease: "power2.out",
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "white",
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+});
+
+img2.forEach(function (img) {
+img.addEventListener("mouseover", function (e) {
+  // console.log("Mouse over image: " + e.target.src);
+  e.target.src =
+    "https://2022-pha5e-website-prod.s3.eu-west-3.amazonaws.com/assets/bigger-science_thumbnail-2.png"; // Change the image source
+  gsap.to(img, {
+    display: "block", // Show the image
+    opacity: 1, // Smoothly fade out the image instead of hiding it
+    duration: 0.3,
+    ease: "power2.out",
+    zIndex: 999, // Move the image to the back to prevent it from being covered by the text
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "transparent", // Make text transparent
+    WebkitTextStroke: "0.5px #4e4d4d", // Add stroke effect
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+
+img.addEventListener("mouseleave", function (e) {
+  e.target.src = " "; // Change the image source
+
+  gsap.to(img, {
+    opacity: 1, // Fade the image back in
+    duration: 0.3,
+    ease: "power2.out",
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "white",
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+});
+
+img3.forEach(function (img) {
+img.addEventListener("mouseover", function (e) {
+  // console.log("Mouse over image: " + e.target.src);
+  e.target.src =
+    "https://2022-pha5e-website-prod.s3.eu-west-3.amazonaws.com/assets/Capture%20da%C3%8CeI%C3%8Ccran%202024-01-04%20aI%C3%8C%2016.41.06-2.png"; // Change the image source
+  gsap.to(img, {
+    display: "block", // Show the image
+    opacity: 1, // Smoothly fade out the image instead of hiding it
+    duration: 0.3,
+    ease: "power2.out",
+    zIndex: 999, // Move the image to the back to prevent it from being covered by the text
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "transparent", // Make text transparent
+    WebkitTextStroke: "0.5px #4e4d4d", // Add stroke effect
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+
+img.addEventListener("mouseleave", function (e) {
+  e.target.src = " "; // Change the image source
+
+  gsap.to(img, {
+    opacity: 1, // Fade the image back in
+    duration: 0.3,
+    ease: "power2.out",
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "white",
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+});
+
+img4.forEach(function (img) {
+img.addEventListener("mouseover", function (e) {
+  // console.log("Mouse over image: " + e.target.src);
+  e.target.src =
+    "https://2022-pha5e-website-prod.s3.eu-west-3.amazonaws.com/assets/Capture-d%C3%A2e%C3%8Ccran-2023-04-03-a%C3%8C-10.59.43_2-2.jpg"; // Change the image source
+  gsap.to(img, {
+    display: "block", // Show the image
+    opacity: 1, // Smoothly fade out the image instead of hiding it
+    duration: 0.3,
+    ease: "power2.out",
+    // Move the image to the back to prevent it from being covered by the text
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "transparent", // Make text transparent
+    WebkitTextStroke: "0.5px #4e4d4d", // Add stroke effect
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+
+img.addEventListener("mouseleave", function (e) {
+  e.target.src = " "; // Change the image source
+
+  gsap.to(img, {
+    opacity: 1, // Fade the image back in
+    duration: 0.3,
+    ease: "power2.out",
+  });
+
+  gsap.to(headings, {
+    fontFamily: "'D', sans-serif", // Apply custom font
+    color: "white",
+    duration: 0.3,
+    ease: "power2.out",
+  });
+});
+});
+
+
+
+const containerClasses = ["image-containers1", "image-containers2", "image-containers3", "image-containers4"];
+
+containerClasses.forEach((className) => {
+document.querySelectorAll(`.${className}`).forEach((container, index) => {
+  container.addEventListener("mousemove", (e) => {
+    let rect = container.getBoundingClientRect();
+    let x = (e.clientX - rect.left) / rect.width - 0.5;
+    let y = (e.clientY - rect.top) / rect.height - 0.5;
+
+    let moveX = x * 40;
+    let moveY = y * 40;
+
+    // Ensure corresponding info element is shown if applicable
+    let infoElements = document.querySelectorAll(`.info${className.slice(-1)}`);
+    if (infoElements.length && infoElements[index]) {
+      infoElements[index].style.display = "block";
+    }
+
+    gsap.to(container, {
+      x: moveX,
+      y: moveY,
+      zIndex: 999,
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  });
+
+  container.addEventListener("mouseleave", () => {
+    let infoElements = document.querySelectorAll(`.info${className.slice(-1)}`);
+    if (infoElements.length && infoElements[index]) {
+      infoElements[index].style.display = "none";
+    }
+
+    gsap.to(container, {
+      x: 0,
+      y: 0,
+      duration: 0.4,
+      ease: "power2.out",
+    });
+  });
+});
+});
+
+}
+
